e9f0873c3ec0673a643ff86105b517daa9fc3668
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-28 14:23:48 +0300

    test buttons

--- App.js
+++ App.js
@@ -1,49 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+const App = () => {
+  const [count, setCount] = useState(0)
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}> Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>{count}</Text>
+        <TouchableHighlight onPress={() => setCount(count + 1)}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Pos</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => setCount(count - 1)}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Neg</Text>
+          </View>
+        </TouchableHighlight>
       </View>
+      
     );
   }
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000000',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 48,
     textAlign: 'center',
+    color: 'greenyellow',
     margin: 10,
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#FFFFFF',
     marginBottom: 5,
   },
+  button: {
+    backgroundColor: 'greenyellow',
+    padding: 10,
+    margin: 5,
+    borderRadius: 20
+
+  },
+  buttonText: {
+    color: 'black'
+  }
 });
+
+export default App
--- documentation/diary.md
+++ documentation/diary.md
@@ -3,7 +3,7 @@
 ### 28th May 2019
 #### Starting with React Native
 
-Initial setup of the project
+##### Initial setup of the project
 ```
 $ brew install watchman
 $ npm install -g react-native-cli
@@ -19,3 +19,16 @@ It also opened Metro Bundler CLI, which monitors for changes in JS files.
 The app opened on my iPhone and then I shaked it to enable live reload. After that
 any change I made in VSCode to App.js file triggered almost instant reload on the iPhone.
 Cool, time to start the actual coding.
+
+##### React Natice specific components
+
+Because Native is a bit different, I took some time to get to know how to use different
+components. Native doesn't use HTML but it's own components that are compiled to
+device specific, native components. For example iOS has UIKit, and every React component
+are actually transpiled to that. So, we have for example View, Text and TextInput components.
+
+Styles in React Native can be applied the same way as normal React projects, with inline code.
+So I also tested that and changed the colors of the app, for starters.
+
+Next I did some very basic counter app that had to buttons and useState hook which
+set the text element according to counter value.
